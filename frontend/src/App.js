7bc95f65641b633879eb930a29bf0ad65d500507
matt-plank <matthew_plank@outlook.com>
2023-05-13 20:04:48 +0100

    Added projects section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import CV from "./components/cv/cv";
+import Projects from "./components/projects/projects";
 import Title from "./components/title/title";
 
 function App() {
   return (
     <>
       <Title />
+      <Projects />
       <CV />
     </>
   );
---
+++ frontend/src/components/projects/projects.js
@@ -0,0 +1,18 @@
+import "./projects.css";
+
+const Projects = () => {
+  return (
+    <div className="projects">
+      <h2>Projects</h2>
+
+      <div className="cards">
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
