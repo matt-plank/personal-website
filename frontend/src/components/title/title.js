89fff8b78b1fc99673d0c97685a7d8b99631cd99
matt-plank <matthew_plank@outlook.com>
2023-05-13 19:44:56 +0100

    Added title area to frontend

---
+++ frontend/src/components/title/title.js
@@ -0,0 +1,14 @@
+import "./title.css";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <div className="content">
+        <h1>Matt Plank</h1>
+        <h2>Software Engineer & Data Scientist</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Title;
