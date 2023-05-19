7cf96674ca95f8bf3c3a7241cf5c0acf99f45bdf
matt-plank <matthew_plank@outlook.com>
2023-05-19 23:32:22 +0100

    Updated styling of title area

--- frontend/src/sections/title/title.js
+++ frontend/src/sections/title/title.js
@@ -1,3 +1,4 @@
+import { SiGithub, SiLinkedin } from "react-icons/si";
 import "./title.css";
 
 const Title = () => {
@@ -6,6 +7,23 @@ const Title = () => {
       <div className="content">
         <h1>Matt Plank</h1>
         <h2>Software Engineer & Data Scientist</h2>
+        <div className="links">
+          <a
+            href="https://www.linkedin.com/in/matt-plank-856668199/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <SiLinkedin />
+          </a>
+
+          <a
+            href="https://www.github.com/matt-plank"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <SiGithub />
+          </a>
+        </div>
       </div>
     </div>
   );
